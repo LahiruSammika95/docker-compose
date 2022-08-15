76aea6167a9c33c43201291a3c023fa140497c19
Lahiru Sammika <65813472+LahiruSammika95@users.noreply.github.com>
2022-08-15 20:53:36 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -36,7 +36,7 @@ app.post('/add-user', function (req, res) {
   res.send(userObj);
 });
 
-app.get('/get-user', function (req, res) {
+app.get('/get-user/:userName', function (req, res) {
   let response = {};
 
   MongoClient.connect(process.env.DB_URL, mongoClientOptions, function (err, client) {
@@ -44,7 +44,7 @@ app.get('/get-user', function (req, res) {
 
     let db = client.db(databaseName);
 
-    let myquery = { userName: req.body.userName };
+    let myquery = { userName: req.params.userName };
 
     db.collection("users").findOne(myquery, function (err, result) {
       if (err) throw err;
